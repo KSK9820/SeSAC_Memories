c1a84b874070fed973a6f0d116075569398bf6bb
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-05-23 17:21:36 +0900

    0523 과제
    TableView 구현(Case 1, 2)

---
+++ 0523/0523/ThirdTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ThirdTableViewController.swift
+//  0523
+//
+//  Created by 김수경 on 5/23/24.
+//
+
+import UIKit
+
+class ThirdTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+}
