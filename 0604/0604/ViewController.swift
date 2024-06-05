aa842002f729ab380db5a32d2e1cba3cc672bf18
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-06-05 19:07:16 +0900

    Code Base 변경을 위해 스토리보드 삭제

--- 0604/0604/ViewController.swift
+++ 0604/0604/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .white
         configureHierarchy()
         configureLayout()
     }
