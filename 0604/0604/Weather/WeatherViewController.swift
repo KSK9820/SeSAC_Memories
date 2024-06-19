d0ae7e8d6063830e218494129bf947ac52a586c1
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-06-20 01:06:28 +0900

    0619 과제
    위치 정보를 기반으로 날씨 정보를 가져오는 기능 추가

--- 0604/0604/Weather/WeatherViewController.swift
+++ 0604/0604/Weather/WeatherViewController.swift
@@ -6,13 +6,14 @@
 //
 
 import UIKit
+import CoreLocation
 import Alamofire
 import SnapKit
 import Kingfisher
 
 final class WeatherViewController: UIViewController {
     
-    var location = "London"
+    let locationManager = CLLocationManager()
     var weatherData: Weather?
     
     private let dateLabel = UILabel()
@@ -25,8 +26,11 @@ final class WeatherViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        getWeatherData()
+        
+        locationManager.delegate = self
+        
+        checkDeviceLocationAuthroization()
+        
         configureHierarchy()
         configureLayout()
         configureUI()
@@ -80,11 +84,11 @@ final class WeatherViewController: UIViewController {
         }
     }
     
-    private func getWeatherData() {
-        let url = APIURL.weather(location).urlString
-        guard let parameter = APIURL.weather(location).parameter else { return }
+    private func getWeatherData(_ coordinate: CLLocationCoordinate2D) {
+        let url = APIURL.currentWeather.urlString
+        let parameter: Parameters = ["lat": coordinate.latitude, "lon": coordinate.longitude, "appid": APIKey.weather.apikey]
         
-        AF.request(url, parameters: parameter).responseDecodable(of: WeatherResponseModel.self) { response in
+        AF.request(url, parameters: parameter, encoding: URLEncoding.queryString).responseDecodable(of: WeatherResponseModel.self) { response in
             switch response.result {
             case .success(let data):
                 self.weatherData = data.weather[0]
@@ -98,7 +102,7 @@ final class WeatherViewController: UIViewController {
     private func setContents() {
         if let weatherData {
             dateLabel.text = Date().formatted()
-            locationLabel.text = location
+
             weatherLabel.text = "today weather is \(weatherData.main)"
             endLabel.text = "have a nice day!"
             
@@ -108,3 +112,111 @@ final class WeatherViewController: UIViewController {
     }
 }
 
+
+extension WeatherViewController {
+    
+    private func checkDeviceLocationAuthroization() {
+        DispatchQueue.global().async {
+            if CLLocationManager.locationServicesEnabled() {
+                DispatchQueue.main.async {
+                    self.checkCurrentLocationAuthroization()
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.showAlert(title: "위치 서비스 불가", message: "위치 서비스가 꺼져있어, 위치 권한을 요청할 수 없습니다.")
+                }
+            }
+        }
+    }
+    
+    private func checkCurrentLocationAuthroization() {
+        var status: CLAuthorizationStatus
+        
+        if #available(iOS 14.0, *) {
+            status = locationManager.authorizationStatus
+        } else {
+            status = CLLocationManager.authorizationStatus()
+        }
+        
+        switch status {
+        case .notDetermined:
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+        case .denied:
+            showAlert(title: "위치 권한 허용 불가", message: "위치 권한이 없습니다. 위치 권한을 허용해주세요.")
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        @unknown default:
+            showAlert(title: "위취 권한 허용 불가", message: "위치 권한이 없습니다.")
+        }
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert)
+        
+        let okay = UIAlertAction(
+            title: "확인",
+            style: .default)
+        
+        alert.addAction(okay)
+        
+        present(alert, animated: true)
+    }
+    
+    func getLocationName(_ location: CLLocationCoordinate2D) {
+        let location = CLLocation(latitude: location.latitude, longitude: location.longitude)
+        
+        CLGeocoder().reverseGeocodeLocation(location) { (placemark, error) in
+            if let error = error {
+                print("Geocoding error: \(error.localizedDescription)")
+                return
+            }
+            
+            if let placemark = placemark?.first {
+                var address = ""
+                if let locality = placemark.locality {
+                    address += locality + " "
+                }
+                if let thoroughfare = placemark.thoroughfare {
+                    address += thoroughfare + " "
+                }
+                if let subThoroughfare = placemark.subThoroughfare {
+                    address += subThoroughfare + " "
+                }
+                
+                DispatchQueue.main.async {
+                    self.locationLabel.text = address
+                }
+            }
+        }
+    }
+}
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let coordinate = locations.last?.coordinate {
+            
+            getWeatherData(coordinate)
+            getLocationName(coordinate)
+        }
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        showAlert(title: "위치 정보 검색 오류", message: "사용자의 위치 정보를 가져오지 못 했습니다.")
+    }
+    
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkDeviceLocationAuthroization()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkDeviceLocationAuthroization()
+    }
+    
+}
