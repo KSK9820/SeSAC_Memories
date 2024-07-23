d64fd9c692b6196111e9a089cdb134bdac839d08
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-07-23 15:45:16 +0900

    Init

---
+++ SeSAC_LSLP/SeSAC_LSLP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_LSLP
+//
+//  Created by 김수경 on 7/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
